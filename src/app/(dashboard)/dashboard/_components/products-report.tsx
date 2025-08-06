b6aa1b312296e3bdd4c004b83c70ab2a3dffbf0b
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-08-06 18:17:53 +0600

    dark mode problem solve

--- src/app/(dashboard)/dashboard/_components/products-report.tsx
+++ src/app/(dashboard)/dashboard/_components/products-report.tsx
@@ -14,7 +14,7 @@ export function ProductsReport({
   data = { thisDay: 30, thisWeek: 60, thisMonth: 85 },
 }: ProductsReportProps) {
   return (
-    <div className="bg-white rounded-lg p-6 shadow-sm">
+    <div className="bg-white dark:bg-gray-500 rounded-lg p-6 shadow-sm">
       <h2 className="text-lg font-semibold text-gray-900 dark:text-black mb-6">{title}</h2>
 
       <div className="flex items-start justify-between ">
